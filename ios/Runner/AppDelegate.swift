aec6534ea13e9805b2bd9a2b5a6358569eaad77b
AdhamEhabAi <adhame962@gmail.com>
2025-10-22 18:07:51 +0300

    ,

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,15 +1,30 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import Firebase
+import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyBu10a4F7HSwT1vooV3pZe3L8-Mo6LzhXI")
+
+    FirebaseApp.configure()
+
+    UNUserNotificationCenter.current().delegate = self
+
+    application.registerForRemoteNotifications()
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // Handle FCM Token Refresh
+  func application(_ application: UIApplication,
+                   didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    Messaging.messaging().apnsToken = deviceToken
   }
 }
